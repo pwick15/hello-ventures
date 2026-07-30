2007e43f0dd5d07f47a64dde1beb5178985d6fb4
Punjaya <punjayawick@gmail.com>
2026-07-31 05:51:02 +1000

    Revert "Fix guide blur to exclude highlighted elements"
    This reverts commit fff8b43b70c02991b55b2f24a2a0c0ac91ed821d.

--- frontend/app.js
+++ frontend/app.js
@@ -108,13 +108,6 @@ const driver = window.driver.js.driver;
 const tour = driver({
   showProgress: true,
   animate: true,
-  onHighlightStarted: () => {
-    document.body.classList.add('tour-active');
-  },
-  onDestroyStarted: () => {
-    document.body.classList.remove('tour-active');
-    tour.destroy();
-  },
   steps: [
     {
       popover: {
